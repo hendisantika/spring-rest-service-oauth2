6b8726ed14db6037020606420263dac48118d82a
Hendi Santika <hendisantika@gmail.com>
2017-10-20 05:11:21 +0700

    #11 - Added Web Security Configuration

---
+++ src/main/java/com/hendisantika/oauth2/config/WebSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.hendisantika.oauth2.config;
+
+import com.hendisantika.oauth2.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-service-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/17/17
+ * Time: 6:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 auth.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+}
