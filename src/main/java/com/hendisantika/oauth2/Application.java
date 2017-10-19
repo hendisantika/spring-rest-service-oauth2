be5e509ee28e301197a796e7b30b9d37baa36ab8
Hendi Santika <hendisantika@gmail.com>
2017-10-19 21:37:58 +0700

    #3 - Added Main App

---
+++ src/main/java/com/hendisantika/oauth2/Application.java
@@ -0,0 +1,23 @@
+package com.hendisantika.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-service-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/17/17
+ * Time: 5:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
