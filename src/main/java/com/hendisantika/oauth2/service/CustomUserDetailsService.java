a96f243c4a507bf4ef3c4c6920b2a23c25742480
Hendi Santika <hendisantika@gmail.com>
2017-10-20 05:09:08 +0700

    #8 - Added Customer User Detail Service

---
+++ src/main/java/com/hendisantika/oauth2/service/CustomUserDetailsService.java
@@ -0,0 +1,83 @@
+package com.hendisantika.oauth2.service;
+
+import com.hendisantika.oauth2.domain.User;
+import com.hendisantika.oauth2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-service-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/17/17
+ * Time: 6:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByLogin(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("User %s does not exist!", username));
+		}
+		return new UserRepositoryUserDetails(user);
+	}
+
+	private final static class UserRepositoryUserDetails extends User implements UserDetails {
+
+		private static final long serialVersionUID = 1L;
+
+		private UserRepositoryUserDetails(User user) {
+			super(user);
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return getRoles();
+		}
+
+		@Override
+		public String getUsername() {
+			return getLogin();
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			return true;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			return true;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			return true;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return true;
+		}
+
+	}
+
+}
