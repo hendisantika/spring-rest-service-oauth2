8eb186e87eef4a3c61f0bbff300d5bf9b4b9bcde
Hendi Santika <hendisantika@gmail.com>
2018-10-16 06:11:53 +0700

    #29 - Upgraded into Spring Boot 1.5.16.RELEASE version

--- src/test/java/com/hendisantika/oauth2/GreetingControllerTest.java
+++ src/test/java/com/hendisantika/oauth2/GreetingControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.web.FilterChainProxy;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,9 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Time: 5:51 AM
  * To change this template use File | Settings | File Templates.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
-//@SpringApplicationConfiguration(classes = Application.class)
 @Configuration
 @EnableAutoConfiguration
 public class GreetingControllerTest {
